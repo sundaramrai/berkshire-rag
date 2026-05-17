d37b9e955e2aff097ccd975d2ff1d50d537c3c6e
Sundaram Rai <sundaramrai02@gmail.com>
2026-05-17 15:46:58 +0530

    Add source links for Berkshire letters and utility functions for year extraction

---
+++ src/lib/berkshire/source-links.ts
@@ -0,0 +1,74 @@
+export const BERKSHIRE_LETTER_SOURCE_LINKS: Record<string, string> = {
+  "1977": "https://drive.google.com/file/d/1mIlS3ZJs38X1ShTK8iRzSg8QYKVLcAlq/view",
+  "1978": "https://drive.google.com/file/d/1_nmwe0-y-eB2gtuWeyFAx-a7q064aJDm/view",
+  "1979": "https://drive.google.com/file/d/1vHlkQzIKEo4Cgocd49Tc-7REGaWTFyOg/view",
+  "1980": "https://drive.google.com/file/d/19H5ZZ6rRcjkpqZhcW-_iTsSEIuYebOVX/view",
+  "1981": "https://drive.google.com/file/d/1h4wso3vIqiO2TxIQe6pTmCq7S7hgxT23/view",
+  "1982": "https://drive.google.com/file/d/1-oNf8Dxca_ifncLJ_pxB8lQH4dN2ylp8/view",
+  "1983": "https://drive.google.com/file/d/12-WbhZPGTe7Br4PyKx4qHywq58JcQ_pM/view",
+  "1984": "https://drive.google.com/file/d/1BiA8iydBfTtpz9JASUdJHIiweIg3DOJS/view",
+  "1985": "https://drive.google.com/file/d/1eZnoQ3-YYJfNi3w5NpAVbC3ZPfqw6P4z/view",
+  "1986": "https://drive.google.com/file/d/1sU3lq1iHBs8uB-KWrY2zdri-lIXiPkMl/view",
+  "1987": "https://drive.google.com/file/d/1LrntCbNwm1d5ekwErDUU7gNkfEUKWMgg/view",
+  "1988": "https://drive.google.com/file/d/1x0WaHf5XIVdoLIQ5rBYTb56NXqGoM3XP/view",
+  "1989": "https://drive.google.com/file/d/1NFFdYTVgjpTsbWfMv7Pad-3KJ3G710eL/view",
+  "1990": "https://drive.google.com/file/d/1gb3kzoeQJnSrWXwT1wTL8dkUIb7V4OSH/view",
+  "1991": "https://drive.google.com/file/d/1iOWtvX4e45lkDFL63b4bZb3F_7qUSKBu/view",
+  "1992": "https://drive.google.com/file/d/1qhRDebrR-HzVhBUdIPAW8MFoC4KwTC1o/view",
+  "1993": "https://drive.google.com/file/d/1TCuw_irImgYIPwWjkKguDCFeUSGU37xa/view",
+  "1994": "https://drive.google.com/file/d/1jyEGbKflbBxHBvMVf3U6wmJfALdWZ2Df/view",
+  "1995": "https://drive.google.com/file/d/1uamd2qA0lVY7vAWkM7FaS6QpVwHcTql0/view",
+  "1996": "https://drive.google.com/file/d/1YQV20zGoNQDKE1ilAKeTFUIXAurHdhq0/view",
+  "1997": "https://drive.google.com/file/d/1iZrrAIkbARot9-dL3XQqOtmFikS3WFGB/view",
+  "1998": "https://drive.google.com/file/d/1LwwFrp0EFopgzhmGzoEJFWzsd72VltIF/view",
+  "1999": "https://drive.google.com/file/d/1Hl5tYzV_NCPfXVzU2qncM0mdwHL0Cnug/view",
+  "2000": "https://drive.google.com/file/d/1x9nghDuPDzqq1aeFfn5jeHJrOVQkqatF/view",
+  "2001": "https://drive.google.com/file/d/1j18xZcuiFQ4A-E3pxFBowLO2C_FpOwE4/view",
+  "2002": "https://drive.google.com/file/d/17zTf7GSxG1jA2b6V0xxgp7lWCD5C3ftN/view",
+  "2003": "https://drive.google.com/file/d/1LX2CQCnx576lMXqmdN3pnaPvTwB_hp1Z/view",
+  "2004": "https://drive.google.com/file/d/1G4BGD1nFBtj11bp5wOI3M5FMTNYweFuw/view",
+  "2005": "https://drive.google.com/file/d/1F8HpAGiyElKNRnjlu4wN4KMtvZ0KW8sV/view",
+  "2006": "https://drive.google.com/file/d/1r4t-AIqXofexItO5AD4pdx1rcYa7B3FX/view",
+  "2007": "https://drive.google.com/file/d/12JGydD5PCQEv6N6wqq7jpZHNEPLWEBor/view",
+  "2008": "https://drive.google.com/file/d/119hhXBmBOIT_ZjNAVo2ET4ehioOvvjTK/view",
+  "2009": "https://drive.google.com/file/d/1uPFI3QF9OyU0BscBf6YJYRw_t6JMn-YH/view",
+  "2010": "https://drive.google.com/file/d/1aIhs0Y3eh6LTZ3Jc7_Ec0LckENIY8679/view",
+  "2011": "https://drive.google.com/file/d/1qgeZoLjhExtGszY_AMSsq_mrGD8HV8yf/view",
+  "2012": "https://drive.google.com/file/d/1UuOheESxaZ7kffjyjhOL8G-t4M9vb7Go/view",
+  "2013": "https://drive.google.com/file/d/1P6R382WkuUN5vqEO0zBKfTCEjjfI04gj/view",
+  "2014": "https://drive.google.com/file/d/1H1LvBZgKFm0yb9-qp-kBCY4XHGKWtARN/view",
+  "2015": "https://drive.google.com/file/d/1yFxXC3Kv2PjUXHgJckrjcen0taE17vrm/view",
+  "2016": "https://drive.google.com/file/d/1uTXmmHe3w_YHmnYDggWZ9gp7KMTLiGuj/view",
+  "2017": "https://drive.google.com/file/d/1sVFcgGVST4Jo2XbIWgE2utWML7NExoDb/view",
+  "2018": "https://drive.google.com/file/d/1E-tq-gf1j4gqp2AE5JXLNutNVkAwXbIe/view",
+  "2019": "https://drive.google.com/file/d/194wQ0IsmAVNUQA41jUqsi1yqUl41ROmr/view",
+  "2020": "https://drive.google.com/file/d/15kSMVr-YIorrxWReM_37ehdj8s_c47md/view",
+  "2021": "https://drive.google.com/file/d/1FMOcgIAoSG1He3sW77HXghubdDuLe6nn/view",
+  "2022": "https://drive.google.com/file/d/16QdUgk8pTkEePK4T0sPcIoTKaCiWwHgz/view",
+  "2023": "https://drive.google.com/file/d/1lbmFJaErVSO8dNZimV_IePTAtP7WMcb9/view",
+  "2024": "https://drive.google.com/file/d/1Pbq7gbkkqlY7m6FoUzMSJUUgddtZ9_KG/view",
+};
+
+export function getBerkshireLetterSourceLink(label: string | undefined) {
+  const year = label ? /(?:19|20)\d{2}/.exec(label)?.[0] : undefined;
+
+  return year ? BERKSHIRE_LETTER_SOURCE_LINKS[year] : undefined;
+}
+
+export function getBerkshireLetterYearsFromText(text: string) {
+  const years = new Set<string>();
+  const yearPattern = /(?:19|20)\d{2}/g;
+  let match = yearPattern.exec(text);
+
+  while (match) {
+    const year = match[0];
+
+    if (BERKSHIRE_LETTER_SOURCE_LINKS[year]) {
+      years.add(year);
+    }
+
+    match = yearPattern.exec(text);
+  }
+
+  return [...years];
+}
